8ee2e49657583032d62408c281cd218c2e93b41f
BlackCatHehe <39700053+BlackCatHehe@users.noreply.github.com>
2025-03-03 16:52:09 +0800

    Update 0.0.3

--- fbac.rb
+++ fbac.rb
@@ -4,10 +4,10 @@
 class Fbac < Formula
   desc "A flutter bloc clean architecture creator"
   homepage "https://github.com/BlackCatHehe/flutter_bloc_clean_architecture_creator"
-  url "https://github.com/BlackCatHehe/flutter_bloc_clean_architecture_creator/releases/download/0.0.2/fbac"
+  url "https://github.com/BlackCatHehe/flutter_bloc_clean_architecture_creator/releases/download/0.0.3/fbac"
   sha256 "062ec324741f948e1e53429f3f044195e6b7d294d70e138050153cf1c2de3b91"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
 
   def install
     bin.install "fbac"
